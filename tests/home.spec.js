1158242666c1b80d712a15350ab6e712d2f61ed3
shruti-40619 <thakurshruti40619@gmail.com>
2026-05-18 22:08:06 +0530

    Implemented homepage multi-tab validation

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -1,11 +1,48 @@
 const { test, expect } = require('@playwright/test');
 
-test('Home page test', async ({ page }) => {
+test('Homepage validations', async ({ page, context }) => {
 
     await page.goto('https://arjitnigam.github.io/myDreams/');
 
-    await expect(
-        page.getByText('My Dreams')
-    ).toBeVisible();
+    // Loader validation
+    const loader = page.locator('.spinner');
+
+    await expect(loader).toBeVisible();
+
+    await expect(loader).toBeHidden();
+
+    // Button visible or not
+    const myDreamsBtn = page.getByText('My Dreams');
+
+    await expect(myDreamsBtn).toBeVisible();
+
+    // Handle two tabs
+    const pagePromise1 = context.waitForEvent('page');
+    const pagePromise2 = context.waitForEvent('page');
+
+    await myDreamsBtn.click();
+
+    const newPage1 = await pagePromise1;
+    const newPage2 = await pagePromise2;
+
+    await newPage1.waitForLoadState();
+    await newPage2.waitForLoadState();
+
+    const urls = [
+      await newPage1.url(),
+      await newPage2.url()
+    ];
+
+    console.log(urls);
+
+    // Verify diary page opened
+    expect(
+       urls.some(url => url.includes('dreams-diary'))
+    ).toBeTruthy();
+
+    // Verify summary page opened
+    expect(
+       urls.some(url => url.includes('dreams-total'))
+    ).toBeTruthy();
 
 });
